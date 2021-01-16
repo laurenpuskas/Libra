2934a9f27ea3de879fcc78cd94acff93bb2901d3
Liliripe <liliripe.studio@gmail.com>
2021-01-16 14:42:18 -0600

    init background animation

--- src/styles/Background.js
+++ src/styles/Background.js
@@ -8,8 +8,28 @@ export const StyledBackground = styled(BackgroundImage)`
     align-items: center;
     position: relative;
 
+    ::before,
+    ::after {
+        background-size: 120%;
+        background-position: center top;
+        animation-name: bgslide;
+        animation-duration: 100s;
+        animation-timing-function: linear;
+        animation-iteration-count: 1;
+    }
     ::after {
         opacity: 44% !important;
+    }
+
+    @keyframes bgslide {
+        from {
+            background-size: 120%;
+            background-position: center top;
+        }
+        to { 
+            background-size: cover;
+            background-position: center bottom;
+        }
     }
 
     .inner {
